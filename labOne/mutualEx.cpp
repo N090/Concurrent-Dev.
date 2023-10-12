3bb496048c41ba168824ee9aac9dcdb5b975ba88
N090 <91678195+N090@users.noreply.github.com>
2023-10-12 16:01:09 +0100

    Update mutualEx.cpp

--- labOne/mutualEx.cpp
+++ labOne/mutualEx.cpp
@@ -1,3 +1,5 @@
+//Author:    Nojus Satikauskas
+
 #include "Semaphore.h"
 #include <iostream>
 #include <thread>
@@ -17,22 +19,28 @@ int sharedVariable=0;
 /*! displays a message that is split in to 2 sections to show how a rendezvous works*/
 void updateTask(std::shared_ptr<Semaphore> firstSem, int numUpdates){
 
- 
+//Call signal
+ firstSem->Wait();
   for(int i=0;i<numUpdates;i++){
-    //UPDATE SHARED VARIABLE HERE!
     sharedVariable++;
   }
-
+//Signal the next thread to start
+firstSem->Signal();
 }
 
 
 int main(void){
   std::vector<std::thread> vt(num_threads);
   std::shared_ptr<Semaphore> aSemaphore( new Semaphore);
+    std::shared_ptr<Semaphore> bSemaphore( new Semaphore);
   /**< Launch the threads  */
   int i=0;
   for(std::thread& t: vt){
     t=std::thread(updateTask,aSemaphore,1000);
+      if(i == (num_threads - 1)){
+        aSemaphore->Signal();
+      }
+      i++;
   }
   std::cout << "Launched from the main\n";
   /**< Join the threads with the main thread */
