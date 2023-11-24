05c2fe22e372c50afb750015b084d2405a398ca5
N090 <91678195+N090@users.noreply.github.com>
2023-11-24 23:12:51 +0000

    Add files via upload

--- labFive/main.cpp
+++ labFive/main.cpp
@@ -1,47 +1,9 @@
-// main.cpp --- 
-// 
-// Filename: main.cpp
-// Description: 
-// Author: Nojus Satikauskas C00261186
-// Maintainer: Nojus Satiakuskas
-// Created: Nov 17 2023
-// Last-Updated: Thur Nov 24 2023
-//           By: Nojus Satikauskas
-// 
-// 
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or (at
-// your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
-// 
-//
-// Code:
-
-/*! \mainpage Concurrency Lab Five
- *
- * \section Description
- *
- * Completed version of Lab Five.
- *
- * Demonstrates a solution to the Dining Philosopher's problem
- *
- * \section p How to Run
- *
- *  main.cpp:
- *
- *  Compiled using g++, run make -k to create
- *
- *  Run ./philosophers to run the program
- *
+/**
+ * @file main.cpp
+ * @author Nojus Satikauskas (C00261186)
+ * @brief Implements the dining philosophers problem using Semaphores for synchronization.
+ * @date 17/11/2023
+ * @copyright GPL-3.0
  */
 
 #include "Semaphore.h"
